090faa7df6af73c5bc7cf03a5fe67b0a8821cd58
Ballack17 <Ballack17@mail.ru>
2020-01-19 23:13:20 +0300

    Первое задание Java 2 курс 1 - добавил класс бэкграунд 2 - добавление шара по левому клику мышки (появляется там где кликнул) 3 - удаление шара по клику правой кнопки мыши( удаляются в обратном порядке)

---
+++ lesson1/DavydenkaDaniilHW1/src/ru/gb/jtwo/lone/online/circles/Background.java
@@ -0,0 +1,31 @@
+package ru.gb.jtwo.lone.online.circles;
+
+import java.awt.*;
+
+public class Background extends Sprite { //не стал фантазировать - цвет генериться сам
+    private  Color color = new Color(
+            (int)(Math.random() * 255),
+            (int)(Math.random() * 255),
+            (int)(Math.random() * 255)
+    );
+    private float deltaTimeBG = 0f; // отсчёт времени до смены
+
+    @Override
+    void update(GameCanvas canvas, float deltaTime) {
+        // частота смены бэкграунда - задана смена в 1 секунду, можно тоже зарандомить разными способами или другие привязки сделать
+        float changeTime = 1f;
+        if (this.deltaTimeBG >= changeTime) {
+            this.color = new Color((int) (Math.random() * 255),
+                    (int) (Math.random() * 255),
+                    (int) (Math.random() * 255)
+            );
+            this.deltaTimeBG = 0;
+        } else {this.deltaTimeBG += deltaTime;}
+    }
+
+    @Override
+    void render(GameCanvas canvas, Graphics g) {
+        g.setColor(color);
+        g.fillRect(canvas.getLeft(), canvas.getTop(), canvas.getWidth(), canvas.getHeight()); //отрисовка по границе окна
+    }
+}
---
+++ lesson1/DavydenkaDaniilHW1/src/ru/gb/jtwo/lone/online/circles/MainCircles.java
@@ -0,0 +1,79 @@
+package ru.gb.jtwo.lone.online.circles;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MainCircles extends JFrame {
+    private static final int POS_X = 400;
+    private static final int POS_Y = 200;
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+
+    private final int MAXBALLS = 1000;  // максимально возможное количество шаров
+    private Ball[] Balls = new Ball[MAXBALLS];   //изменил Sprites на конкретно шарики, посколько у меня уже 2 класса наследников от Sprite
+    private int currentLength = 10;  // стартовое количество шаров
+    private Background backGround = new Background();  // добавление отдельного объекта Бэкграунд
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainCircles();
+            }
+        });
+    }
+
+    private MainCircles() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
+        initApplication();
+        GameCanvas canvas = new GameCanvas(this);
+        add(canvas, BorderLayout.CENTER);
+        canvas.addMouseListener(new MouseAdapter() {
+           // ДОбавил событие нажатия кнопки
+           @Override
+           public void mousePressed(MouseEvent e) {
+               if (e.getButton() == MouseEvent.BUTTON1) {
+                   if (currentLength < MAXBALLS) { //где кликнул там и появляется шарик
+                       Balls[currentLength] = new Ball(e.getX (), e.getY());
+                       currentLength += 1;
+                   }
+               }
+               if (e.getButton() == MouseEvent.BUTTON3) {
+                   if(currentLength > 0) //не стал уже писать выводы сообщений типа - нет шаров или уже макс шаров на экране..
+                       currentLength -= 1;
+               }
+           }
+        });
+        setTitle("Circles");
+        setVisible(true);
+
+    }
+
+    private void initApplication() {
+        for (int i = 0; i < currentLength; i++) {
+            Balls[i] = new Ball();
+        }
+    }
+
+    public void onDrawFrame(GameCanvas canvas, Graphics g, float deltaTime) {
+        update(canvas, deltaTime); // obnovlenie // S = v * t
+        render(canvas, g); // otrisovka
+    }
+
+    private void update(GameCanvas canvas, float deltaTime) {
+        for (int i = 0; i < currentLength; i++) {
+            Balls[i].update(canvas, deltaTime);
+        }
+        backGround.update(canvas, deltaTime);
+    }
+
+    private void render(GameCanvas canvas, Graphics g) {
+        backGround.render(canvas, g);
+        for (int i = 0; i < currentLength; i++) {
+            Balls[i].render(canvas, g);
+        }
+    }
+}
